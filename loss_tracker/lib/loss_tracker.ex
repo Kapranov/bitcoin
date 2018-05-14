ab2a38dc5803d8cd9b6d81f9929cc213977e07aa
Oleg G.Kapranov <lugatex@yahoo.com>
2018-05-14 10:46:59 +0300

    generated plug project

---
+++ loss_tracker/lib/loss_tracker.ex
@@ -0,0 +1,18 @@
+defmodule LossTracker do
+  @moduledoc """
+  Documentation for LossTracker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LossTracker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ loss_tracker/test/loss_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule LossTrackerTest do
+  use ExUnit.Case
+  doctest LossTracker
+
+  test "greets the world" do
+    assert LossTracker.hello() == :world
+  end
+end
