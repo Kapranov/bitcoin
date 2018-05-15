227c328c52559baa8bb48c511c8ea1fe10800f20
Oleg G.Kapranov <lugatex@yahoo.com>
2018-05-15 10:18:40 +0300

    added analysis tools

--- bitcoin_address/config/config.exs
+++ bitcoin_address/config/config.exs
@@ -1,3 +1,13 @@
 use Mix.Config
 
+if Mix.env == :dev do
+  config :mix_test_watch,
+    clear: true
+end
+
+# Notification Types
+config :ex_unit_notifier, notifier: ExUnitNotifier.Notifiers.NotifySend
+# config :ex_unit_notifier, notifier: ExUnitNotifier.Notifiers.TerminalNotifier
+# config :ex_unit_notifier, notifier: ExUnitNotifier.Notifiers.TerminalTitle
+
 # import_config "#{Mix.env}.exs"
--- bitcoin_address/mix.exs
+++ bitcoin_address/mix.exs
@@ -4,9 +4,20 @@ defmodule BitcoinAddress.MixProject do
   def project do
     [
       app: :bitcoin_address,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
       deps: deps()
     ]
   end
@@ -17,7 +28,27 @@ defmodule BitcoinAddress.MixProject do
     ]
   end
 
+  defp description do
+    """
+    The generate Bitcoin addresses using Elixir
+    """
+  end
+
+  defp package do
+    [
+      contributors: "LugaTeX",
+      liscences: "MIT",
+      links: %{"Github" => "https://github.com/kapranov/bitcoin/bitcoin_address"}
+    ]
+  end
+
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.18.3", only: :dev, runtime: false},
+      {:credo, "~> 0.9.2", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.8", only: :test},
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
+      {:ex_unit_notifier, "~> 0.1.4", only: :test}
+    ]
   end
 end
